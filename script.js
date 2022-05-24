deeddaa2d635a211bffc162916864d4394136eea
husseinalkarmi <husseinalkarmi012@gmail.com>
2022-05-24 14:55:57 +0300

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let dom = document.getElementById('dom');
+
+// dom.innerHTML = dom;
+function myFunction(dom) {
+    
+    // alert( dom.innerHTML );
+    dom.innerHTML = 'dom';
+  }
+
+// var page1 = document.getElementById('page1').innerHTML;
+
+//  var page2 = document.getElementById('page2');
+
+
+//  page2.innerHTML = page1;
